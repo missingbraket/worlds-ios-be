a7b1d68def202966485432be0dbdf30e4410141b
seohallo <seoohaii@gmail.com>
2025-05-29 18:33:49 +0900

    DTO 생성

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @ApiProperty({ description: '이메일', example: 'test@example.com' })
   email: string;
 
-  @IsNotEmpty()
-  @ApiProperty({ description: '닉네임', example: 'test' })
-  nickname: string;
-
   @MinLength(6)
   @ApiProperty({ description: '비밀번호', example: 'password' })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  role: string;
+
+  @IsNotEmpty()
+  @ApiProperty({ description: '이름', example: 'test' })
+  name: string;
+
 }
