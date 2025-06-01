3b51e5c0bde038ff289010fa0a60838d634e127e
seohallo <seoohaii@gmail.com>
2025-06-01 22:53:51 +0900

    Add: 질문 모듈 생성

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuestionController],
   providers: [QuestionService]
 })
