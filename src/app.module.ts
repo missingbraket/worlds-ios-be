3d72dfe2e5bd0400c11b9cb1727f214289e6ce96
seohallo <seoohaii@gmail.com>
2025-06-02 11:53:00 +0900

    Feat: Azure연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 import { QuestionModule } from './question/question.module';
 import { AnswerModule } from './answer/answer.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AzureStorageModule } from './azure-storage/azure-storage.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule,
-    PrismaModule,
+    ScheduleModule.forRoot({}),
+    UsersModule,
     QuestionModule,
-    AnswerModule
+    AnswerModule,
+    PrismaModule,
+    AuthModule,
+    AzureStorageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
